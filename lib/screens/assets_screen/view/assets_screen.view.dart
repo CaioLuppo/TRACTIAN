cc0d6efc5ae92970424f933806fa209431547011
CaioLuppo <luppocode@outlook.com>
2024-09-10 04:21:33 -0300

    fix: add dispose

--- lib/screens/assets_screen/view/assets_screen.view.dart
+++ lib/screens/assets_screen/view/assets_screen.view.dart
@@ -112,4 +112,11 @@ class _AssetsScreenState extends State<AssetsScreen> {
       ],
     );
   }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    viewModel.dispose();
+    super.dispose();
+  }
 }
